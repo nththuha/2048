ed7e0b3ab0ec0c5ed48e52d338dcce3bbd4db26e
nththuha <thuhango0204@gmail.com>
2025-04-30 21:00:32 +0700

    chore: init ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { resolver, theme } from '@/configs/themes'
+import { resolver, theme } from '@/configs'
 import '@/styles/globals.scss'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import GameView from './components/GameView'
 
 const root = document.getElementById('root')
 root &&
@@ -12,7 +13,7 @@ root &&
     <React.Suspense>
       <BrowserRouter>
         <MantineProvider theme={theme} cssVariablesResolver={resolver}>
-          2048
+          <GameView />
         </MantineProvider>
       </BrowserRouter>
     </React.Suspense>,
