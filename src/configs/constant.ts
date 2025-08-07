6307a99a4a6ca6cfeca98efbb3914d7be5498ccd
nththuha <thuhango0204@gmail.com>
2025-08-07 12:33:34 +0700

    chore: change animation duration

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -1,9 +1,9 @@
 export const SIZE = 4
 export const INITIAL_TILES = 2
 export const WINNING_TILE = 2048
-export const SWIPE_THRESHOLD = 5
+export const SWIPE_THRESHOLD = 2
 export const GITHUB_REPO_2048 = 'https://github.com/nththuha/2048'
-export const MERGE_ANIMATION_DURATION = 100
-export const MOVE_ANIMATION_DURATION = 100
+export const MERGE_ANIMATION_DURATION = 50
+export const MOVE_ANIMATION_DURATION = 50
 export const GAP = 10
 export const WIDTH = 80
