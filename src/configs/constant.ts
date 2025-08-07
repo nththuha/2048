a84c873433b0e5bb320d2c156f5bc3f2ed996948
nththuha <thuhango0204@gmail.com>
2025-08-07 12:30:16 +0700

    chore: change swipe threshold

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -1,7 +1,7 @@
 export const SIZE = 4
 export const INITIAL_TILES = 2
 export const WINNING_TILE = 2048
-export const SWIPE_THRESHOLD = 15
+export const SWIPE_THRESHOLD = 5
 export const GITHUB_REPO_2048 = 'https://github.com/nththuha/2048'
 export const MERGE_ANIMATION_DURATION = 100
 export const MOVE_ANIMATION_DURATION = 100
